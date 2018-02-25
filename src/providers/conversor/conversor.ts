74d1d3a1fe3460c9444708919be4eddad062abbe
leonardo <leooff@live.com>
2018-02-25 15:31:08 -0300

    change Conversor

--- src/providers/conversor/conversor.ts
+++ src/providers/conversor/conversor.ts
@@ -1,20 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Number } from '../cpu/number';
 
 @Injectable()
 export class ConversorProvider {
     toBinary(num: string){
-        console.log(parseInt(num, 2));
-        console.log(parseInt(num, 2).toString());
-        return parseInt(num, 2).toString();
+        console.log("R:"+(+num).toString(2));
+        return (+num).toString(2);
     }
-    toDecimal(num: string){
-        console.log(parseInt(num, 2));
-        console.log(parseInt(num, 2).toString());
-        return parseInt(num, 10).toString();
+    toDecimal(num: string,base:number){
+        console.log("R:"+parseInt(num,base).toString());
+        return parseInt(num,base).toString();
     }
     toHexadecimal(num: string){
-        console.log(parseInt(num, 2));
-        console.log(parseInt(num, 2).toString());        
-        return parseInt(num, 16).toString();
+        console.log("R:"+(+num).toString(16));        
+        return (+num).toString(16);
     }
 }
