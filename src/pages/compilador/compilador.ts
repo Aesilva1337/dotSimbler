e7c6a724005fd43ae54635157cf1e0a2490cca91
leonardo <leooff@live.com>
2018-03-05 20:18:24 -0300

    merge

--- src/pages/compilador/compilador.ts
+++ src/pages/compilador/compilador.ts
@@ -1,10 +1,11 @@
-import { Analyzer } from './../../providers/analyzer/analyzer';
-import { CpuProvider } from './../../providers/cpu/cpu';
-import { RegistradorGeralProvider } from './../../providers/registrador-geral/registrador-geral';
-import { Component,Output } from '@angular/core';
+import { LineOperation } from './../../providers/cpu/lineOperation';
+import { CPU } from './../../providers/cpu/cpu';
+import { Registrador } from './../../providers/cpu/registrador/registrador';
+import { Conversor } from './../../providers/conversor/conversor';
+import { Component, Output } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ConversorProvider } from '../../providers/conversor/conversor';
-import { RegistradorEstadoProvider } from '../../providers/registrador-estado/registrador-estado';
+import { Analyzer } from '../../providers/analyzer/analyzer';
+import { ToastController } from 'ionic-angular/components/toast/toast-controller';
 
 /**
  * Generated class for the CompiladorPage page.
@@ -23,71 +24,95 @@ export class CompiladorPage {
   tipoEntrada: string = "2";
   tipoSaida: string = "1";
   rangeSpeed: string = "250";
-
   regAx: string = "00000000";
   regBx: string = "00000000";
   regCx: string = "00000000";
   regDx: string = "00000000";
-  conversor: ConversorProvider;
-
-  text:string = "";
-
-
+  text: string = "";
   val = 1;
+  line:any = ["x"];
 
-  constructor(public navCtrl: NavController, 
-    public navParams: NavParams, 
-    public conversorProvider: ConversorProvider,
-    public reg:RegistradorGeralProvider,
-    public cpu:CpuProvider,
-    private anl:Analyzer,
-    public regE:RegistradorEstadoProvider) {
-    this.conversor = this.conversorProvider;
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public conversor: Conversor,
+    public reg: Registrador,
+    public cpu: CPU,
+    private anl: Analyzer,
+    public toastCtrl: ToastController) {
+
+  }
+   lineop;
+
+  public error(msg) {
+      let toast = this.toastCtrl.create({
+        message: "Error: "+msg,
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
+  }
+ 
+
+  lineUp() {
+    this.line = this.text.split('\n');
   }
 
-
-   sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  prox(){
+    this.cpu.proximaOperation(this);
+  }
+  voltar(){
+    this.cpu.voltarOperation(this);
+  }
+  parar(){
+    this.cpu.parar();
   }
 
-
-  async play(){
+  async play() {
     let result = this.anl.analyzer(this.text);
-    let lineop = result[0];
+    this.lineop = result[0];
     let lineopErro = result[1];
-    this.cpu.mapMemory(lineop);
-    for(let i=0;i<lineop.length; i++){
-      this.cpu.next();
-      this.changeSaida();
-      await this.sleep(+this.rangeSpeed*10); 
+
+    console.log(lineopErro.length);
+    if(lineopErro.length > 0){
+        document.getElementById(""+lineopErro[0].line)
+        .classList.add("erro") ;
+        document.getElementById(""+lineopErro[0].line)
+        .addEventListener('click',(e)=>{
+           this.error(lineopErro[0].msgErro);
+        });
+    }else{
+      this.cpu.setLineOperations(this.lineop);
+      this.cpu.processarOperations(this);
     }
-      
+
+
   }
 
-  LEFTPAD(value:string){
-    return "00000000".substr(value.length)+value;
+  LEFTPAD(value: string) {
+    return "00000000".substr(value.length) + value;
   }
-  changeSaida(){
-    switch(this.tipoSaida){
-      case "0":{
-        this.regAx = this.conversor.toDecimal(this.reg.DX.getValue(),2);
-        this.regBx = this.conversor.toDecimal(this.reg.CX.getValue(),2);
-        this.regCx = this.conversor.toDecimal(this.reg.BX.getValue(),2);
-        this.regDx = this.conversor.toDecimal(this.reg.DX.getValue(),2);
+  changeSaida() {
+    switch (this.tipoSaida) {
+      case "0": {
+        this.regAx = this.conversor.toDecimal(this.reg.DX, 2);
+        this.regBx = this.conversor.toDecimal(this.reg.CX, 2);
+        this.regCx = this.conversor.toDecimal(this.reg.BX, 2);
+        this.regDx = this.conversor.toDecimal(this.reg.DX, 2);
         break;
       }
-      case "1":{
-        this.regAx = this.LEFTPAD(this.conversor.toBinary(this.reg.AX.getValue()));
-        this.regBx = this.LEFTPAD(this.conversor.toBinary(this.reg.BX.getValue()));
-        this.regCx = this.LEFTPAD(this.conversor.toBinary(this.reg.CX.getValue()));
-        this.regDx = this.LEFTPAD(this.conversor.toBinary(this.reg.DX.getValue()));
+      case "1": {
+        this.regAx = this.LEFTPAD(this.conversor.toBinary(this.reg.AX));
+        this.regBx = this.LEFTPAD(this.conversor.toBinary(this.reg.BX));
+        this.regCx = this.LEFTPAD(this.conversor.toBinary(this.reg.CX));
+        this.regDx = this.LEFTPAD(this.conversor.toBinary(this.reg.DX));
         break;
       }
-      case "2":{
-        this.regAx = this.conversor.toHexadecimal(this.reg.AX.getValue()).toUpperCase();
-        this.regBx = this.conversor.toHexadecimal(this.reg.BX.getValue()).toUpperCase();
-        this.regCx = this.conversor.toHexadecimal(this.reg.CX.getValue()).toUpperCase();
-        this.regDx = this.conversor.toHexadecimal(this.reg.DX.getValue()).toUpperCase();
+      case "2": {
+        this.regAx = this.conversor.toHexadecimal(this.reg.AX).toUpperCase();
+        this.regBx = this.conversor.toHexadecimal(this.reg.BX).toUpperCase();
+        this.regCx = this.conversor.toHexadecimal(this.reg.CX).toUpperCase();
+        this.regDx = this.conversor.toHexadecimal(this.reg.DX).toUpperCase();
         break;
       }
     }
