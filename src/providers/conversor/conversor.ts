cb0d3124294e054496384adc7beafdb46b88e506
aesilva1337 <arthur.elidio.ae@gmail.com>
2018-03-12 20:02:47 -0300

    Criação de classes e limitação dos registradores para 8 bits apenas

--- src/providers/conversor/conversor.ts
+++ src/providers/conversor/conversor.ts
@@ -3,12 +3,12 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class Conversor {
     toBinary(num){
-        return (+num).toString(2);
+        return (+num).toString(2).substring(0,7);
     }
     toDecimal(num,base:number){
-        return parseInt(num,base).toString();
+        return parseInt(num,base).toString().substring(0,7);
     }
     toHexadecimal(num){     
-        return (+num).toString(16);
+        return (+num).toString(16).substring(0,7);
     }
 }
